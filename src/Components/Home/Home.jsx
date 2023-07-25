b68a4ca643e4e0938d36e43c01c24533798621b3
lilendian0x00 <Joshuacol115@proton.me>
2023-07-25 01:24:54 +0100

    Updated text sizes of the home page's navigation bar

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -51,14 +51,15 @@ function Home() {
             <div className='flex flex-col text-[var(--creamy)] h-[95%] p-3 w-[90%] border-solid rounded-xl bg-[var(--creamy)] dark:bg-[var(--wash-black)] shadow-lg shadow-black'>
 
                 <div className='flex flex-row items-center text-center'>
-                    <h1 className='mr-auto text-3xl font-bold text-[var(--wash-black)] dark:text-[var(--creamy)]'>{page.charAt(0).toUpperCase() + page.slice(1)}</h1>
+                    <h1 className='mr-auto text-3xl md:text-4xl font-bold text-[var(--wash-black)] dark:text-[var(--creamy)]'>{page.charAt(0).toUpperCase() + page.slice(1)}</h1>
+                    
                     <div className='flex flex-row outline-none border-solid border-2 border-[var(--creamy)] rounded-xl bg-transparent'>
 
-                        <Link to={"/home/stats"} onClick={() => setPage("stats")} className={`flex flex-col outline-none justify-center border-solid border-[var(--wash-black)] border-2 rounded-xl text-sm font-bold w-20 h-7 transition duration-150 ease-in-out hover:ease-in ${(page === "stats") ? "bg-[#D9D9D9] text-[#0D0D0D]" : "text-[#D9D9D9]"}`} >
+                        <Link to={"/home/stats"} onClick={() => setPage("stats")} className={`flex flex-col outline-none justify-center border-solid border-[var(--wash-black)] border-2 rounded-xl text-sm md:text-xl font-bold w-20 h-7 transition duration-150 ease-in-out hover:ease-in ${(page === "stats") ? "bg-[#D9D9D9] text-[#0D0D0D]" : "text-[#D9D9D9]"}`} >
                             <span>stats</span>
                         </Link>
 
-                        <Link to={"/home/xray"} onClick={() => setPage("xray")} className={`flex flex-col outline-none justify-center border-solid border-[var(--wash-black)] border-2 rounded-xl text-sm font-bold w-20 h-7 transition duration-150 ease-in-out hover:ease-in ${(page === "xray") ? "bg-[#D9D9D9] text-[#0D0D0D]" : "text-[#D9D9D9]"}`} >
+                        <Link to={"/home/xray"} onClick={() => setPage("xray")} className={`flex flex-col outline-none justify-center border-solid border-[var(--wash-black)] border-2 rounded-xl text-sm md:text-xl font-bold w-20 h-7 transition duration-150 ease-in-out hover:ease-in ${(page === "xray") ? "bg-[#D9D9D9] text-[#0D0D0D]" : "text-[#D9D9D9]"}`} >
                             <span>xray</span>
                         </Link>
 
