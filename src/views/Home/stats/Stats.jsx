8ce0a59f50189bdc5b6f488d3446a08b2c3f7422
lilendian0x00 <Joshuacol115@proton.me>
2023-07-30 10:37:25 +0100

    Started xray section

--- src/views/Home/stats/Stats.jsx
+++ src/views/Home/stats/Stats.jsx
@@ -6,25 +6,23 @@ import { AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai';
 import { BsFillCpuFill, BsArrowLeftRight } from 'react-icons/bs';
 
 
-const pages = {
-    "stats": {
-        "cpu": {
-            "model": "i9-13900K",
-            "cores": 24,
-            "usage": 0.24,
-            "load_avg": "0.74 | 0.65 | 0.58" // top -n 1 | awk 'NR==1 { for (i = 13; i <= NF; i++) { printf "%s ", $i } printf "\n" }' FS=" " OFS=" "
-        },
-        "ram": {
-            "total": 128,
-            "inuse": 8.24
-        },
-        "network": {
-            "total_recv": 104,      // Gigabytes
-            "total_send": 79,       // Gigabytes
-            "downlink": 23.12,      // Megabit
-            "uplink": 7.46,         // Megabit
-            "fds": 1045,
-        }
+const data = {
+    "cpu": {
+        "model": "i9-13900K",
+        "cores": 24,
+        "usage": 0.24,
+        "load_avg": "0.74 | 0.65 | 0.58" // top -n 1 | awk 'NR==1 { for (i = 13; i <= NF; i++) { printf "%s ", $i } printf "\n" }' FS=" " OFS=" "
+    },
+    "ram": {
+        "total": 128,
+        "inuse": 8.24
+    },
+    "network": {
+        "total_recv": 104,      // Gigabytes
+        "total_send": 79,       // Gigabytes
+        "downlink": 23.12,      // Megabit
+        "uplink": 7.46,         // Megabit
+        "fds": 1045,
     }
 }
 
@@ -33,7 +31,7 @@ function Stats() {
         query: '(min-width: 768px)'
     });
 
-    const [stats, setStats] = useState(pages)
+    const [stats, setStats] = useState(data)
 
     return (
         <motion.div
@@ -44,23 +42,23 @@ function Stats() {
             transition={{ duration: 0.5 }}
         >
             <div role='status' className='flex flex-col md:flex-row md:justify-center md:items-center gap-4 text-lg mt-2'>
-                <GaugeBox id="CPU" name={isDesktopOrLaptop ? "CPU" : "CPU"} value={`${stats.stats.cpu.model} - ${stats.stats.cpu.cores.toString()}c`} percentage={(stats.stats.cpu.usage)} />
+                <GaugeBox id="CPU" name={isDesktopOrLaptop ? "CPU" : "CPU"} value={`${stats.cpu.model} - ${stats.cpu.cores.toString()}c`} percentage={(stats.cpu.usage)} />
 
 
-                <GaugeBox id="RAM" name={isDesktopOrLaptop ? "RAM" : "RAM"} value={`${stats.stats.ram.inuse.toString()}GB / ${stats.stats.ram.total.toString()}GB`} percentage={(stats.stats.ram.inuse / stats.stats.ram.total)} />
+                <GaugeBox id="RAM" name={isDesktopOrLaptop ? "RAM" : "RAM"} value={`${stats.ram.inuse.toString()}GB / ${stats.ram.total.toString()}GB`} percentage={(stats.ram.inuse / stats.ram.total)} />
 
             </div>
 
             <div className='flex flex-row p-4 justify-center mt-3 md:gap-4'>
-                <SimpleBox className="w-1/2" icon={AiOutlineArrowDown} size={24} name={"Download"} value={`${stats.stats.network.downlink} Mb/s`} />
-                
-                <SimpleBox className="w-1/2" icon={AiOutlineArrowUp} size={24} name={"Upload"} value={`${stats.stats.network.uplink}  Mb/s`} />
+                <SimpleBox className="w-1/2" icon={AiOutlineArrowDown} size={24} name={"Download"} value={`${stats.network.downlink} Mb/s`} />
+
+                <SimpleBox className="w-1/2" icon={AiOutlineArrowUp} size={24} name={"Upload"} value={`${stats.network.uplink}  Mb/s`} />
             </div>
 
             <div className='flex flex-row p-4 justify-center mt-1 md:gap-4'>
-                <SimpleBox className="w-1/2" valueClassName="text-sm" icon={BsArrowLeftRight} size={20} name={"FDs"} value={`${stats.stats.network.fds}`} />
+                <SimpleBox className="w-1/2" valueClassName="text-sm" icon={BsArrowLeftRight} size={20} name={"FDs"} value={`${stats.network.fds}`} />
 
-                <SimpleBox className="w-1/2" valueClassName="text-sm" icon={BsFillCpuFill} size={20} name={"Load"} value={`${stats.stats.cpu.load_avg}`} />
+                <SimpleBox className="w-1/2" valueClassName="text-sm" icon={BsFillCpuFill} size={20} name={"Load"} value={`${stats.cpu.load_avg}`} />
 
             </div>
 
--- src/views/Home/xray/Xray.jsx
+++ src/views/Home/xray/Xray.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import { useState } from 'react'
 import { motion } from 'framer-motion'
 
+const data = {
+  "xray": {
+    "status": "active",
+    "pid": "1218451",
+    "memory": "41.3M",
+    "uptime": "5 days ago",
+  }
+}
+
 function Xray() {
+  const [xray, setXrayStatus] = useState(data)
+
   return (
     <motion.div
-      className=''
+      className='p-1 md:p-4'
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
-      
+
+      <div className='flex flex-col items-center mt-4'>
+        <span className='text-2xl'>Xray Service</span>
+      </div>
+
     </motion.div>
   )
 }
